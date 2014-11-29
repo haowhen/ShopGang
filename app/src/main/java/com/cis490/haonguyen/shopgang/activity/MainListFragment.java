04425055bd0081890ed8305ae00115c5153f0918
AB244 <ajbenn04@louisville.edu>
2014-11-28 20:20:41 -0500

    Add main view
    Added listview, and items. Adaptor to follow.

---
+++ app/src/main/java/com/cis490/haonguyen/shopgang/activity/MainListFragment.java
@@ -0,0 +1,11 @@
+package com.cis490.haonguyen.shopgang.activity;
+
+import android.app.Fragment;
+
+/**
+ * Created by Alex on 11/28/2014.
+ */
+public class MainListFragment extends Fragment {
+
+
+}
