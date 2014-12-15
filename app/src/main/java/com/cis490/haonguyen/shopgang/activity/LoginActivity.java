9fc13b49b62ed57e2fe597ded7e8abe1e5cfd1dd
haowhen <hnnguy02@louisville.edu>
2014-12-15 17:19:09 -0500

    Friendslist stuff done

--- app/src/main/java/com/cis490/haonguyen/shopgang/activity/LoginActivity.java
+++ app/src/main/java/com/cis490/haonguyen/shopgang/activity/LoginActivity.java
@@ -85,8 +85,6 @@ public class LoginActivity extends FragmentActivity {
 		navDrawerItems.add(new NavDrawerItem(navMenuTitles[0], navMenuIcons.getResourceId(0, -1)));
 		navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], navMenuIcons.getResourceId(1, -1)));
 		navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons.getResourceId(2, -1)));
-		navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons.getResourceId(3, -1)));
-		navDrawerItems.add(new NavDrawerItem(navMenuTitles[4], navMenuIcons.getResourceId(4, -1)));
 		navMenuIcons.recycle();
 
 		adapter = new NavDrawerListAdapter(getApplicationContext(), navDrawerItems);
@@ -259,20 +257,14 @@ public class LoginActivity extends FragmentActivity {
 				break;
 
 			case 1:
-
+				fragment = new FriendListFragment();
 				break;
 
 			case 2:
-				break;
-
-			case 3:
-				fragment = new FriendListFragment();
-				break;
-			case 4:
 				fragment = new LogOutFragment();
 				break;
-			default:
 
+			default:
 				break;
 
 		}
